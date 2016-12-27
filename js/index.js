fa2de90fa22df4fa4499a37fcaa91186ecbf532a
Kalleo Pinheiro <kalleopinheiro@hotmail.com>
2016-12-27 12:44:32 -0200

    inclusão de wow, animate.css e efeitos na página index

--- js/index.js
+++ js/index.js
@@ -4,10 +4,13 @@ $('.toggle').click(function(){
           $(this).toggleClass('ativo');
         });
 
+$(function(){
+	new WOW().init();
+});
 
-    $(document).ready(function($) {
-      $(".scroll").click(function(event){
-      event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-      });
-    });
+$(document).ready(function($) {
+  $(".scroll").click(function(event){
+  event.preventDefault();
+  $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+  });
+});
